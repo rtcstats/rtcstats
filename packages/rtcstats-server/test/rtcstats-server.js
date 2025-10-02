8871452f9ebe5002d422eebaacd880f9e5b25258
Philipp Hancke <philipp.hancke@googlemail.com>
2025-10-02 07:46:54 +0200

    improve server test coverage (in particular for JWT)

--- packages/rtcstats-server/test/rtcstats-server.js
+++ packages/rtcstats-server/test/rtcstats-server.js
@@ -1,11 +1,74 @@
 import config from 'config';
+import {WebSocket} from 'ws';
 
 import {RTCStatsServer} from '../rtcstats-server.js';
+import {generateAuthToken, setupDirectory} from '../utils.js';
 
 describe('RTCStatsServer', () => {
+    beforeEach(() => {
+        // Setup up work and upload directories.
+        setupDirectory(config, config.server.workDirectory);
+        setupDirectory(config, config.server.uploadDirectory);
+    });
+
+    let server;
     it('starts and stops', async () => {
-        const server = new RTCStatsServer(config);
+        server = new RTCStatsServer(config);
         await server.listen();
         await server.close();
+    });
+
+    it('connects and disconnects a websocket', async () => {
+        const confWithoutAuth = structuredClone(config);
+        confWithoutAuth.authorization = {};
+        server = new RTCStatsServer(confWithoutAuth);
+        await server.listen();
+
+        const ws = new WebSocket('ws://localhost:' + config.server.httpPort);
+        const openPromise = new Promise(resolve => {
+            ws.on('open', () => {
+                ws.close();
+                resolve();
+            });
+        });
+        await openPromise;
+        await server.close();
+    });
+
+    describe('JWT authorization', () => {
+        const jwtSecret = 'testSecret';
+        it('rejects a websocket if authorization is configured', async () => {
+            const confWithAuth = structuredClone(config);
+            confWithAuth.authorization = {jwtSecret};
+            server = new RTCStatsServer(confWithAuth);
+            await server.listen();
+
+            const ws = new WebSocket('ws://localhost:' + config.server.httpPort);
+            const closePromise = new Promise(resolve => {
+                ws.on('close', (code) => {
+                    resolve(code);
+                });
+            });
+            expect(await closePromise).to.equal(1008);
+            await server.close();
+        });
+
+        it('accepts a websocket with a valid token', async () => {
+            const confWithAuth = structuredClone(config);
+            confWithAuth.authorization = {jwtSecret};
+            server = new RTCStatsServer(confWithAuth);
+            await server.listen();
+
+            const token = await generateAuthToken({user: 'testUser'}, jwtSecret);
+            const ws = new WebSocket('ws://localhost:' + config.server.httpPort + '?rtcstats-token=' + token);
+            const openPromise = new Promise(resolve => {
+                ws.on('open', () => {
+                    ws.close();
+                    resolve();
+                });
+            });
+            await openPromise;
+            await server.close();
+        });
     });
 });
