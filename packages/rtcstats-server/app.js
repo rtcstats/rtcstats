34fe1354ee521e18314eb0f6243ab9160f1f6998
Philipp Hancke <philipp.hancke@googlemail.com>
2025-11-11 12:49:06 +0100

    server: add uncaught exception handler
    which might have surfaced the issue from #90 earlier.

--- packages/rtcstats-server/app.js
+++ packages/rtcstats-server/app.js
@@ -1,11 +1,20 @@
+import process from 'node:process';
+
 import config from 'config';
 
 import {RTCStatsServer} from './rtcstats-server.js';
 import {setupDirectory} from './utils.js';
 
-// Setup up work and upload directories.
+// Setup work and upload directories.
 setupDirectory(config, config.server.workDirectory);
 setupDirectory(config, config.server.uploadDirectory);
+
+// Setup unhandled exception handler in production.
+if (process.env.NODE_ENV === 'production') {
+    process.on('uncaughtException', (err, origin) => {
+    console.error('Caught exception:', err, 'Exception origin:', origin);
+  });
+}
 
 const server = new RTCStatsServer(config);
 server.listen();
