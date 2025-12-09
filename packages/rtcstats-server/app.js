98873fe135aab7ea00b05850f7ebc77d68bd977d
Philipp Hancke <philipp.hancke@googlemail.com>
2025-12-09 18:11:03 +0100

    server: add cluster support
    based on
      https://nodejs.org/api/cluster.html
    Previously, rtcstats-server was doing combined collection and feature extraction.
    Since it is now only responsible for collection of dumps and uploading enable parallelism.
    
    Deployment should be done on instances with an appropriate number of CPUs.

--- packages/rtcstats-server/app.js
+++ packages/rtcstats-server/app.js
@@ -1,3 +1,5 @@
+import cluster from 'node:cluster';
+import os from 'node:os';
 import process from 'node:process';
 
 import config from 'config';
@@ -15,6 +17,14 @@ if (process.env.NODE_ENV === 'production') {
     console.error('Caught exception:', err, 'Exception origin:', origin);
   });
 }
+const numProc = process.env.NODE_ENV === 'production' ? os.availableParallelism() : 1;
 
-const server = new RTCStatsServer(config);
-server.listen();
+if (cluster.isPrimary) {
+    console.log('Primary process running with', numProc, 'child processes');
+    for (let i = 0; i < numProc; i++) {
+        cluster.fork();
+    }
+} else {
+    const server = new RTCStatsServer(config);
+    server.listen();
+}
